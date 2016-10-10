e690e78c6ecc40525c923db023a1acfbd5b44951
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-10 15:41:15 +0330

    Adds a docker ip to table

--- database/migrations/2016_10_10_120217_add_columns_to_user_table.php
+++ database/migrations/2016_10_10_120217_add_columns_to_user_table.php
@@ -13,7 +13,7 @@ class AddColumnsToUserTable extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('docker_ip');
         });
     }
@@ -25,7 +25,7 @@ class AddColumnsToUserTable extends Migration
      */
     public function down()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('docker_ip');
         });
     }
