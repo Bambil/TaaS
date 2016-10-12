6b5dcc64cb6a3f931a81b70519e20f5278a67f77
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-12 12:41:43 +0330

    Adds middleware name config

--- config/middleware.php
+++ config/middleware.php
@@ -2,6 +2,7 @@
 
 return [
     'db_host' => env('MIDDLEWARE_DB_HOST', '127.0.0.1'),
+    'name' => env('MIDDLEWARE_NAME', 'I1820'),
     'image_name' => env('MIDDLWARE_IMAGE_NAME', 'aolab/i1820'),
     'image_tag' => env('MIDDLEWARE_IMAGE_TAG', 'latest'),
     'db_port' => env('MIDDLEWARE_DB_PORT', '8086'),
