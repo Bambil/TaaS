c3794ea459c04e81f18f9f98c159fe3a4aaf3ab5
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-12 11:54:43 +0330

    Add middleware configuration

---
+++ config/middleware.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db_host' => env('MIDDLEWARE_DB_HOST', '127.0.0.1'),
+    'image_name' => env('MIDDLWARE_IMAGE_NAME', 'aolab/i1820'),
+    'image_tag' => env('MIDDLEWARE_IMAGE_TAG', 'latest'),
+    'db_port' => env('MIDDLEWARE_DB_PORT', '8086'),
+    'db_username' => env('MIDDLEWARE_DB_USERNAME', 'root'),
+    'db_password' => env('MIDDLEWARE_DB_PASSWORD', 'root'),
+    'image' => env('MIDDLWARE_IMAGE_NAME', 'aolab/i1820') . ':' . env('MIDDLEWARE_IMAGE_TAG', 'latest')
+];
