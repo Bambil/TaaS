538b1e731d4358e46f46f65d76adee82ed536a9e
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-12 12:42:37 +0330

    Adds basic running of a container

--- app/DockerHelper.php
+++ app/DockerHelper.php
@@ -1,24 +1,46 @@
 <?php
 
 /**
- * Created by PhpStorm.
  * User: iman
  * Date: 10/10/16
  * Time: 3:48 PM
  */
 
+use Docker\API\Model\ContainerConfig;
+use Docker\API\Model\HostConfig;
 use Docker\Docker;
+use Webpatser\Uuid\Uuid;
 
 class DockerHelper
 {
     private $docker;
-    function create() {
+
+    function __construct()
+    {
         $this->docker = new Docker();
     }
 
-    function createNewI1820() {
-        $this->docker->getImageManager()->create(null, [
-            'fromImage' => "aolab/i1820:latest"
-        ]);
+    function getManager() {
+        return $this->docker->getContainerManager();
     }
+
+    function createNewMiddleware() {
+        $containerConfig = new ContainerConfig();
+        $containerConfig->setImage(config('middleware.image'));
+
+        if(config('middleware.name') == 'I1820') {
+            $containerConfig->setEnv('I1820_INFLUXDB_HOST', config('middleware.db_host'));
+        }
+
+        // Create and run container
+        $containerCreateResult = $this->getManager()->create($containerConfig);
+        $this->getManager()->start($containerCreateResult->getId());
+
+        return $containerCreateResult->getId();
+    }
+
+    function findContainerIp() {
+
+    }
+
 }
