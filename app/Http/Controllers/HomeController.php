5bd3e9a994496219655a156134dfcaf2053cfec6
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-12 17:47:25 +0330

    Minor bug fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,6 +25,6 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return redirect()->action('NodeController@show', ['id', $user->docker_id]);
+        return redirect()->action('NodeController@show', [$user->docker_id]);
     }
 }
