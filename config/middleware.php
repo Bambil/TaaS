7d3faaa6493665932fdc2440a72639e342ee17a4
Iman Tabrizian <Tabrizian@outlook.com>
2016-10-16 23:30:01 +0330

    Adds mqtt to config

--- config/middleware.php
+++ config/middleware.php
@@ -8,5 +8,7 @@ return [
     'db_port' => env('MIDDLEWARE_DB_PORT', '8086'),
     'db_username' => env('MIDDLEWARE_DB_USERNAME', 'root'),
     'db_password' => env('MIDDLEWARE_DB_PASSWORD', 'root'),
-    'image' => env('MIDDLWARE_IMAGE_NAME', 'aolab/i1820') . ':' . env('MIDDLEWARE_IMAGE_TAG', 'latest')
+    'image' => env('MIDDLWARE_IMAGE_NAME', 'aolab/i1820') . ':' . env('MIDDLEWARE_IMAGE_TAG', 'latest'),
+    'mqtt_host' => env('MQTT_HOST', '127.0.0.1'),
+    'mqtt_port' => env('MQTT_PORT', '1883')
 ];
