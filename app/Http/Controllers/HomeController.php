87e8d1b138c033d0e3da3d241d4275519080a0a2
Iman Tabrizian <Tabrizian@outlook.com>
2016-11-08 12:39:17 +0330

    Get compatible with new model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
-        return redirect()->action('NodeController@show', [$user->docker_id]);
+        return view('home');
     }
 }
