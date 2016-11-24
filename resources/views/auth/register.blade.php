4ba40c2611a61cdb7310081c10c75024867de6eb
Iman Tabrizian <Tabrizian@outlook.com>
2016-11-24 20:22:19 +0330

    We are using vue components to build our awesome login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
 <login>
-    <email></email>
-    <password></password>
-    <checkbox></checkbox>
-    <submit></submit>
+    {{ csrf_field() }}
+    <email>
+        E-Mail Address
+        @if ($errors->has('email'))
+                <error slot="errors">{{ $errors->first('email') }}</error>
+        @endif
+    </email>
+    <password>
+        Password
+        @if ($errors->has('password'))
+                <error slot="errors">{{ $errors->first('password') }}</error>
+        @endif
+    </password>
+    <checkbox>
+        Remember me
+     </checkbox>
+    <submit>Login</submit>
 </login>
 @endsection
